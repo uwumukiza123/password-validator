9b065e2b7064e0f060c885bc883f0a979e92a42f
uwumukiza123 <uwumukizalaetitia@gmail.com>
2023-10-17 11:37:05 +0200

    modifying password validator

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -7,34 +7,23 @@ export const forbiddenPasswords = ["amG84h6yeQ", "mc9Q20pdjH", "jnT6Q2f8U5"];
  * @returns {boolean}
  */
 export default function isValidPassword(password = "") {
-  // The following line ensures, that password is always a string, like the number 128 -> string "128"
-  // if (typeof password !== "string") password = String(password);
-
-  if (password.length !== 10) return false;
-
-  // check if password is digits or number
-  const isDigitsOrNumber = /[^a-zA-Z0-9]+$/;
-  // checking if there is special characters or not
-  // const specialChar = /^[a-zA-Z0-9\s!$%^&*()_+|~=`{}\[\]:";'<>?,.\\-]+$/;
-  const upperCase = /[A-Z]+$/;
-  const lowerCase = /[a-z]+$/;
-  const upperOrLower = /^[A-Za-z]+$/;
-  // const sequence = /^(?!.*(012|123|234|345|456|567|678|789)|(\d)\1{2,}).*$/;
-  // const sequence =
-  //   /^(?!.*(?:012|123|234|345|456|567|678|789))(?=.*[a-zA-Z]).*$/;
-  const sequence =
-    /(012|123|234|345|456|567|678|789|987|876|765|654|543|432|321|210)/;
-
-  if (isDigitsOrNumber.test(password)) return false;
-  // if (!specialChar.test(password)) return false;
-  if (!sequence.test(password)) return false;
-  if (upperOrLower.test(password)) return false;
+  if (typeof password !== "string") password = String(password);
   if (forbiddenPasswords.includes(password)) return false;
-  if (!upperCase.test(password) || !lowerCase.test(password)) return false;
-
+  // check if password does't contain special characters
+  if (!/^[a-zA-Z0-9]{10}$/.test(password)) return false;
+  if (!/[A-Z]/.test(password)) return false;
+  if (!/[a-z]/.test(password)) return false;
+  if (!/[0-9]/.test(password)) return false;
+  // check if password is ascending or descending 
+  if (
+    /(?:0123|2345|3456|5678|6789|7890|0987|9876|8765|7654|6543|5432|4321|3210)/.test(
+      password
+    )
+  )
+    return false;
   const setOfPassword = new Set([...password]);
-  console.log({ password });
   if (setOfPassword.size < 4) return false;
 
   return true;
 }
+
